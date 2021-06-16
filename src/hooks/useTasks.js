0f478152b94658f40d15c1c8b06e1eb17a32217c
xzotacx <jesus.152006@gmail.com>
2021-06-16 18:05:37 -0400

    cambiando ruta

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { TodoConnection } from "../services/todoServices";
 import { v4 as uuidv4 } from "uuid";
-import { URI } from "../congif";
+import { URI } from "../config";
 export const useTask = (id, _tasks) => {
   const Conn = new TodoConnection(URI);
   const [tasks, setTasks] = useState(_tasks);
